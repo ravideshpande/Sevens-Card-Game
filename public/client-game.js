d2e3576b0d2b8aad1d296f2247b192291f235ce5
root <rohanvdeshpande@hotmail.com>
2018-09-10 20:53:11 -0400

    Shuffle Cards and display

--- public/client-game.js
+++ public/client-game.js
@@ -1,5 +1,7 @@
 var socket = io.connect("http://localhost:4000");
 
+var deck = [];
+
 function setName(){
 	var playerName = document.getElementById("playerName").value;
 	if(playerName!="Enter Name..." && playerName!=""){
@@ -24,4 +26,49 @@ socket.on('listPlayers',function(data){
 		output+= "<li>"+data.names[i]+"</li>";
 	}
 	document.getElementById("gamePlayers").innerHTML = output;
+});
+
+socket.on('startHand',function(data){
+	document.getElementById("gameRoom").style.display = "none";
+	console.log(data);
+	deck = data;
+	for(var i = 0; i<deck.length;i++){
+		var output = "";
+ 		var first = "<div class='card'><div class='value'>";
+ 		var middle1 = "</div><div class='suit "
+
+ 		var middle2= "'></div><div class='rotatedValue'>";
+ 		var end = "</div></div>";
+
+ 		var value = deck[i].value;
+ 		var suit = deck[i].suit;
+ 		var suitString = "";
+ 		if(suit == 0){
+ 			suitString = "heart";
+ 		}
+ 		else if(suit == 1){
+ 			suitString = "spade";
+ 		}
+ 		else if(suit == 2){
+ 			suitString = "club";
+ 		}
+ 		else{
+ 			suitString = "diamond"
+ 		}
+ 		if(value == 11){
+ 			value = "J"
+ 		}
+ 		else if(value == 12){
+ 			value = "Q"
+ 		}
+ 		else if(value == 13){
+ 			value = "K" 
+ 		}
+ 		else if(value == 1){
+ 			value = "A"
+ 		}
+ 		output = first + value + middle1 + suitString + middle2 + value + end;
+
+ 		document.getElementById("gameTable").innerHTML += output;
+	}
 });
--- server.js
+++ server.js
@@ -14,6 +14,21 @@ var io = socket(server);
 var allPlayers = [];
 var addPlayers = true;
 
+var deck = [];
+for(var i = 0; i < 4; i++) {
+	for(var j = 1; j <= 13; j++) {
+		var card = {
+			suit:i,
+			value:j
+		}
+		deck.push(card)
+	}
+}
+
+for(var i = 0; i<5; i++){
+	deck = shuffle(deck)
+}
+
 
 io.on('connection',function(socket){
 	console.log('made socket connection', socket.id);
@@ -22,6 +37,7 @@ io.on('connection',function(socket){
 			playerID:allPlayers.length,
 			connectionID:socket.id,
 			playerName:"",
+			hand : []
 		}
 		allPlayers.push(tempPlayer);
 	}
@@ -46,6 +62,27 @@ io.on('connection',function(socket){
 			console.log("The Game has Started!");
 			addPlayers = false;
 			console.log(allPlayers);
+
+			var NUM_PLAYERS = allPlayers.length;
+
+			for(var i = 0; i < 52; i++){
+		 		var playerIndex = (i % NUM_PLAYERS);
+		 		allPlayers[playerIndex].hand.push(deck[i]);
+		 	}
+
+		 	for(var i = 0; i<NUM_PLAYERS;i++){
+		 		io.to(allPlayers[i].connectionID).emit('startHand',allPlayers[i].hand);
+		 	}
 		}
 	});
 });
+
+function shuffle(input){
+	var shuffled = [];
+	for(var i = input.length;i>0; i--){
+		var randomIndex = Math.floor(Math.random()*i);
+		shuffled.push(input[randomIndex]);
+		input.splice(randomIndex,1);
+	}
+	return shuffled;
+} 
